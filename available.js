57e78d95d1584adcc1f7ee20384306c153356a0c
nigh7sh4de <plotnikd@gmail.com>
2016-05-14 14:09:41 -0400

    Implement remove time range

--- available.js
+++ available.js
@@ -1,6 +1,7 @@
 
 var available = function() {
     this.time = [];
+    this.cycles = 0;
 }
 
 available.prototype = {
@@ -10,7 +11,7 @@ available.prototype = {
     },
     
     checkTime: function(val) {
-        for (var i = 0; i < this.time.length; i++) {
+        for (var i = 0; i < this.time.length; i+=2) {
             var min = this.time[i];
             var max = this.time[i+1];
             if (min <= val && val <= max)
@@ -23,14 +24,28 @@ available.prototype = {
     
     removeTimeRange: function(start, end) {
         for (var i=0; i < this.time.length; i+=2) {
-            if (this.time[i+1] > start) {
+            this.cycles++;
+            if (this.time[i+1] >= start) {
                 if (this.time[i] > end)
                     return;
-                if (this.time[i] < start &&
-                    this.time[i+1] > end) {
-                        this.time.insert(start - 1/*ms*/, i + 1);
-                        this.time.insert(end, i + 2)
-                    }
+                else if (this.time[i] < start &&
+                         this.time[i+1] > end) {
+                    this.time.splice(i + 1, 0, start - 1/*ms*/, end);
+                    return;
+                }
+                else if (this.time[i] >= start &&
+                         this.time[i+1] < end) {
+                    this.time.splice(i, 2);
+                    i -= 2;
+                }
+                else if (this.time[i] < start &&
+                         this.time[i+1] < end) {
+                    this.time[i+1] = start - 1/*ms*/;
+                }
+                else if (this.time[i] >= start &&
+                         this.time[i+1] > end) {
+                    this.time[i] = end;
+                }
             }
         }
     }
