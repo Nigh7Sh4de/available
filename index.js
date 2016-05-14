82788eadc260bab024217df14a3bd29dba164457
nigh7sh4de <plotnikd@gmail.com>
2016-05-14 14:10:18 -0400

    Update runner to check new features

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+var check = function() {
+    for(var i=1;i<=10;i++)
+        console.log(i + ' ' + a.checkTime(i));
+}
+
 var Available = require('./available');
 var a = new Available();
-a.addTimeRange(2, 4);
+a.addTimeRange(2, 5);
+a.addTimeRange(5, 6);
+a.addTimeRange(5, 6);
+a.addTimeRange(5, 6);
+a.addTimeRange(5, 6);
+a.addTimeRange(5, 6);
+a.addTimeRange(5, 8);
 
-for(var i=1;i<6;i++)
-    console.log(a.checkTime(i));
-    
-console.log(a.cycle);
+
+// check();
+
+a.removeTimeRange(4,6);
+
+check();
+
+console.log('Done: ' + a.cycles);
