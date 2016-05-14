5e65db209d735af4d4bea026ea5aa30eae81173d
nigh7sh4de <plotnikd@gmail.com>
2016-05-14 19:06:19 -0400

    Added some stuff, broke some stuff

--- available.js
+++ available.js
@@ -4,10 +4,26 @@ var available = function() {
     this.cycles = 0;
 }
 
+// available.prototype.
+
 available.prototype = {
+    _dec: function(d) {
+        return new Date(d.valueOf() - 1);
+    },
     addTimeRange: function(start, end) {
-        this.time.push(start);
-        this.time.push(end - 1 /*ms*/);
+        var _start = null;
+        var count = 0;
+        var found = false;
+        for (var i=0; i < this.time.length; i+=2) {
+            this.cycles++;
+            if (this.time[i+1] >= this._dec(start)) {
+                count ++;
+                _start = i;
+                start = this.time[i];
+            }
+        }
+        _start = _start != null ? _start : this.time.length;
+        this.time.splice(_start, 2 * count, start, this._dec(end));
     },
     
     checkTime: function(val) {
@@ -29,7 +45,7 @@ available.prototype = {
                     return;
                 else if (this.time[i] < start &&
                          this.time[i+1] > end) {
-                    this.time.splice(i + 1, 0, start - 1/*ms*/, end);
+                    this.time.splice(i + 1, 0, this._dec(start), end);
                     return;
                 }
                 else if (this.time[i] >= start &&
@@ -39,7 +55,7 @@ available.prototype = {
                 }
                 else if (this.time[i] < start &&
                          this.time[i+1] < end) {
-                    this.time[i+1] = start - 1/*ms*/;
+                    this.time[i+1] = this._dec(start);
                 }
                 else if (this.time[i] >= start &&
                          this.time[i+1] > end) {
--- index.js
+++ index.js
@@ -5,18 +5,31 @@ var check = function() {
 
 var Available = require('./available');
 var a = new Available();
+
+a._dec = function(n) {
+    return n - 1;
+} 
+
+// a.addTimeRange(new Date('2016/01/01 2:00'), new Date('2016/01/01 5:00'));
+// a.addTimeRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
+// a.addTimeRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
+// a.addTimeRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
+// a.addTimeRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
+// a.addTimeRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
+// a.addTimeRange(new Date('2016/01/01 6:00'), new Date('2016/01/01 8:00'));
 a.addTimeRange(2, 5);
 a.addTimeRange(5, 6);
 a.addTimeRange(5, 6);
 a.addTimeRange(5, 6);
 a.addTimeRange(5, 6);
 a.addTimeRange(5, 6);
-a.addTimeRange(5, 8);
+a.addTimeRange(9, 10);
+a.addTimeRange(6, 8);
 
 
-// check();
+check();
 
-a.removeTimeRange(4,6);
+a.removeTimeRange(4, 6);
 
 check();
 
