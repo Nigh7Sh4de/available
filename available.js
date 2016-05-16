2a3ec2966d38798d41f785642db760f5853f45cc
Dennis Plotnik <adknowledgeuser@android-74633fd597416a80.adknowledge.com>
2016-05-16 11:58:21 -0400

    Back in the green

--- available.js
+++ available.js
@@ -1,29 +1,77 @@
 
-var available = function() {
-    this.time = [];
+var available = function(arr, type) {
+    this.time = arr || [];
     this.cycles = 0;
+    if (!this.type)
+        setType(type || 'number');
 }
 
-// available.prototype.
+var setType = function(type) {
+    var self = available.prototype;
+    if (this instanceof available)
+        self = this;
+    self.type = type;
+    self.dec = available.types[type].dec;
+}
+
+available.types = {
+    date: {
+        dec: function(d) {
+            return new Date(d.valueOf() - 1);
+        }
+    },
+    number: {
+        dec: function(n) {
+            return n - 1;
+        }
+    }
+}
+
+available.setType = setType; 
 
 available.prototype = {
-    _dec: function(d) {
-        return new Date(d.valueOf() - 1);
+    type: null,
+    dec: function() {
+        console.error('Type not set.');
+        return new Error('Type not set.');
     },
+    setType: setType, 
     addTimeRange: function(start, end) {
-        var _start = null;
-        var count = 0;
-        var found = false;
+        end = this.dec(end);
+        var index = null,
+            count = 0;
         for (var i=0; i < this.time.length; i+=2) {
             this.cycles++;
-            if (this.time[i+1] >= this._dec(start)) {
-                count ++;
-                _start = i;
-                start = this.time[i];
+            if (this.time[i] > end) {
+                break;
+            }
+            else if (this.time[i+1] >= this.dec(start)) {
+                if (this.time[i] <= start &&
+                    this.time[i+1] >= end) {
+                    return;
+                }
+                else if (this.time[i] >= start &&
+                         this.time[i+1] < end) {
+                    if (index === null) index = i;
+                    count++;
+                }
+                else if (this.time[i] < start &&
+                         this.time[i+1] <= end) {
+                    if (index === null) index = i;
+                    count++;
+                    start = this.time[i];
+                }
+                else if (this.time[i] >= start &&
+                         this.time[i+1] >= end) {
+                    if (index === null) index = i;
+                    count++;
+                    end = this.time[i+1];
+                    break;;
+                }
             }
         }
-        _start = _start != null ? _start : this.time.length;
-        this.time.splice(_start, 2 * count, start, this._dec(end));
+        index = index === null ? this.time.length : index;
+        this.time.splice(index, 2 * count, start, end);
     },
     
     checkTime: function(val) {
@@ -45,7 +93,7 @@ available.prototype = {
                     return;
                 else if (this.time[i] < start &&
                          this.time[i+1] > end) {
-                    this.time.splice(i + 1, 0, this._dec(start), end);
+                    this.time.splice(i + 1, 0, this.dec(start), end);
                     return;
                 }
                 else if (this.time[i] >= start &&
@@ -55,7 +103,7 @@ available.prototype = {
                 }
                 else if (this.time[i] < start &&
                          this.time[i+1] < end) {
-                    this.time[i+1] = this._dec(start);
+                    this.time[i+1] = this.dec(start);
                 }
                 else if (this.time[i] >= start &&
                          this.time[i+1] > end) {
