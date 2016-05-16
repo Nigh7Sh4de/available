5825315e81943f7150fd75726410ceef0b346bd1
Dennis Plotnik <adknowledgeuser@android-74633fd597416a80.adknowledge.com>
2016-05-16 12:10:15 -0400

    Rename to `ranger`

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
 var check = function() {
     for(var i=1;i<=10;i++)
-        console.log(i + ' ' + a.checkTime(new Date('2016/01/01 ' + i + ':00')));
-        // console.log(i + ' ' + a.checkTime(i));
+        console.log(i + ' ' + a.check(new Date('2016/01/01 ' + i + ':00')));
+        // console.log(i + ' ' + a.check(i));
 }
 
-var Available = require('./available');
-var a = new Available();
+var Ranger = require('./ranger');
+var a = new Ranger();
 
-a.addTimeRange(new Date('2016/01/01 2:00'), new Date('2016/01/01 5:00'));
-a.addTimeRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
-a.addTimeRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
-a.addTimeRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
-a.addTimeRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
-a.addTimeRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
-a.addTimeRange(new Date('2016/01/01 9:00'), new Date('2016/01/01 10:00'));
-a.addTimeRange(new Date('2016/01/01 6:00'), new Date('2016/01/01 8:00'));
-// a.addTimeRange(2, 4);
-// a.addTimeRange(5, 6);
-// a.addTimeRange(5, 6);
-// a.addTimeRange(5, 6);
-// a.addTimeRange(5, 6);
-// a.addTimeRange(5, 6);
-// a.addTimeRange(9, 10);
-// a.addTimeRange(6, 8);
-// console.log(a.time);
-// a.addTimeRange(2, 7);
+a.addRange(new Date('2016/01/01 2:00'), new Date('2016/01/01 5:00'));
+a.addRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
+a.addRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
+a.addRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
+a.addRange(new Date('2016/01/01 5:00'), new Date('2016/01/01 6:00'));
+a.addRange(new Date('2016/01/01 9:00'), new Date('2016/01/01 10:00'));
+a.addRange(new Date('2016/01/01 6:00'), new Date('2016/01/01 8:00'));
+// a.addRange(2, 4);
+// a.addRange(5, 6);
+// a.addRange(5, 6);
+// a.addRange(5, 6);
+// a.addRange(5, 6);
+// a.addRange(5, 6);
+// a.addRange(9, 10);
+// a.addRange(6, 8);
+// console.log(a.ranges);
+// a.addRange(2, 7);
 
 
 check();
 
-a.removeTimeRange(new Date('2016/01/01 4:00'), new Date('2016/01/01 6:00'));
-// a.removeTimeRange(4, 6);
+a.removeRange(new Date('2016/01/01 4:00'), new Date('2016/01/01 6:00'));
+// a.removeRange(4, 6);
 
 check();
 
 console.log('Done.');
 // console.log('Done: ' + a.cycles);
-console.log(a.time);
+console.log(a.ranges);
---
+++ ranger.js
@@ -0,0 +1,83 @@
+
+var ranger = function(arr, type) {
+    this.ranges = arr || [];
+}
+
+ranger.prototype = {
+    addRange: function(start, end) {
+        var index = null,
+            count = 0;
+        for (var i=0; i < this.ranges.length; i+=2) {
+            if (this.ranges[i] > end) {
+                break;
+            }
+            else if (this.ranges[i+1] >= start) {
+                if (this.ranges[i] <= start &&
+                    this.ranges[i+1] >= end) {
+                    return;
+                }
+                else if (this.ranges[i] >= start &&
+                         this.ranges[i+1] < end) {
+                    if (index === null) index = i;
+                    count++;
+                }
+                else if (this.ranges[i] < start &&
+                         this.ranges[i+1] <= end) {
+                    if (index === null) index = i;
+                    count++;
+                    start = this.ranges[i];
+                }
+                else if (this.ranges[i] >= start &&
+                         this.ranges[i+1] >= end) {
+                    if (index === null) index = i;
+                    count++;
+                    end = this.ranges[i+1];
+                    break;;
+                }
+            }
+        }
+        index = index === null ? this.ranges.length : index;
+        this.ranges.splice(index, 2 * count, start, end);
+    },
+    
+    check: function(val) {
+        for (var i = 0; i < this.ranges.length; i+=2) {
+            var min = this.ranges[i];
+            var max = this.ranges[i+1];
+            if (min <= val && val < max)
+                return true;
+            if (min > val)
+                return false;
+        }
+        return false;
+    },
+    
+    removeRange: function(start, end) {
+        for (var i=0; i < this.ranges.length; i+=2) {
+            if (this.ranges[i+1] >= start) {
+                if (this.ranges[i] > end)
+                    return;
+                else if (this.ranges[i] < start &&
+                         this.ranges[i+1] > end) {
+                    this.ranges.splice(i + 1, 0, start, end);
+                    return;
+                }
+                else if (this.ranges[i] >= start &&
+                         this.ranges[i+1] < end) {
+                    this.ranges.splice(i, 2);
+                    i -= 2;
+                }
+                else if (this.ranges[i] < start &&
+                         this.ranges[i+1] < end) {
+                    this.ranges[i+1] = start;
+                }
+                else if (this.ranges[i] >= start &&
+                         this.ranges[i+1] > end) {
+                    this.ranges[i] = end;
+                }
+            }
+        }
+    }
+}
+
+module.exports = ranger;
