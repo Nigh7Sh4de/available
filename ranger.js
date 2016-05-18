a1d42a04eb42ebb9f51e771b43dec69ef0d2cc3e
Dennis Plotnik <adknowledgeuser@android-74633fd597416a80.adknowledge.com>
2016-05-18 11:01:41 -0400

    Implement recuring ranges and fix removeRange cases

--- ranger.js
+++ ranger.js
@@ -10,6 +10,46 @@ var endBeforeStart = function(start, end) {
 }
 
 ranger.prototype = {
+    removeRecuringRange: function(start, end, interval, count, finish) {
+        var _start = Number(start),
+            _end = Number(end),
+            _finish = Number(finish),
+            _interval = Number(interval),
+            type = start.constructor;
+        if (endBeforeStart(start, end))
+            return new Error('Start must be before end for range.');
+        if (interval < end - start)
+            return new Error('Interval must be greater than duration of each range.');
+        if ((count == null || count < 0) && finish == null)
+            return new Error('Recuring range must specify total count or limit.');
+        count = count < 1 || !count ? parseInt((_finish - _start) / _interval) : count;
+        if (count <= 0)
+            return new Error('Recuring interval must be greater than 0.');
+        for (var i=0; i < count; i++)
+            this.removeRange(new type(_start + i*_interval), 
+                          new type(_end + i*_interval));
+    },
+    
+    addRecuringRange: function(start, end, interval, count, finish) {
+        var _start = Number(start),
+            _end = Number(end),
+            _finish = Number(finish),
+            _interval = Number(interval),
+            type = start.constructor;
+        if (endBeforeStart(start, end))
+            return new Error('Start must be before end for range.');
+        if (interval < end - start)
+            return new Error('Interval must be greater than duration of each range.');
+        if ((count == null || count < 0) && finish == null)
+            return new Error('Recuring range must specify total count or limit.');
+        count = count < 1 || !count ? parseInt((_finish - _start) / _interval) : count;
+        if (count <= 0)
+            return new Error('Recuring interval must be greater than 0.');
+        for (var i=0; i < count; i++)
+            this.addRange(new type(_start + i*_interval), 
+                          new type(_end + i*_interval));
+    },
+    
     addRange: function(start, end) {
         if (endBeforeStart(start, end))
             return new Error('Start must be before end for range');
@@ -76,7 +116,7 @@ ranger.prototype = {
             return new Error('Start must be before end for range');
         for (var i=0; i < this.ranges.length; i+=2) {
             if (this.ranges[i+1] >= start) {
-                if (this.ranges[i] > end)
+                if (this.ranges[i] >= end)
                     return;
                 else if (this.ranges[i] < start &&
                          this.ranges[i+1] > end) {
@@ -84,12 +124,12 @@ ranger.prototype = {
                     return;
                 }
                 else if (this.ranges[i] >= start &&
-                         this.ranges[i+1] < end) {
+                         this.ranges[i+1] <= end) {
                     this.ranges.splice(i, 2);
                     i -= 2;
                 }
                 else if (this.ranges[i] < start &&
-                         this.ranges[i+1] < end) {
+                         this.ranges[i+1] <= end) {
                     this.ranges[i+1] = start;
                 }
                 else if (this.ranges[i] >= start &&
